d99d26083944bad3ab71bf78615e42fc4c5f9356
William Lopez <GDWilliamLopez@outlook.com>
2017-05-05 16:09:01 -0400

    Original Upload

---
+++ Assets/Scripts/Player Related/Ground_Check_Script.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground_Check_Script : MonoBehaviour {
+
+    //public
+    public bool Ground = false; // Checks for player on ground
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            Ground = true;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            Ground = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ground")
+        {
+            Ground = false;
+        }
+    }
+}
